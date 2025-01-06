39f13addd7bd322d2b046389ee43118ce500c47a
Adil6055 <adil6055@outlook.com>
2025-01-06 23:17:14 +0300

    .

---
+++ test_17/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Test-17',
+    'author': 'Adil',
+    'license': 'LGPL-3',
+    # 'version': '17.0.0',
+
+
+    'category': 'Generic Modules',
+    'sequence': 2,
+   
+    'summary': 'student Management App',
+    'live_test_url': 'https://www.youtube.com/watch?v=0kaHMTtn7oY',
+    'description': "Student Management",
+    'website': 'https://www.github.com/adil6055',
+    
+    'application': True
+}
