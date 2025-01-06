095cca3a276705920c9ce63999b7ae65ab36024a
adil6055 <adil6055@outlook.com>
2025-01-06 16:05:19 +0300

    3.23132

---
+++ hospital_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Student Management',
+    'category': 'Generic Modules/Human Resources',
+    'version': '1.0.0',
+    'sequence': 1,
+    'author': 'Adil',
+    'summary': 'student Management App',
+    'live_test_url': 'https://www.youtube.com/watch?v=0kaHMTtn7oY',
+    'description': "Odoo 18 Payroll, Payroll Odoo 18, Odoo Community Payroll",
+    'website': 'https://www.odoomates.tech',
+    'license': 'LGPL-3',
+    'application': True
+}
