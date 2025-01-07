b101085908ff014378252e6b5274ae2a87457e25
Adil6055 <adil6055@outlook.com>
2025-01-07 22:29:41 +0300

    .

--- hospital_management/__manifest__.py
+++ hospital_management/__manifest__.py
@@ -2,15 +2,15 @@
     'name': 'Hospital Management',
     'category': 'Generic Modules',
     'version': '17.0.1.1',
-    "data" : [
-
-    ]
+    "data": [
+        "views/menu.xml"
+    ],
     'sequence': 1,
     'author': 'Adil',
     'summary': 'Hospital Management App',
     # 'live_test_url': 'https://www.youtube.com/watch?v=0kaHMTtn7oY',
-    # 'description': "Hospital Management",
-    # 'website': 'https://www.github.com/adil6055',
-    # 'license': 'LGPL-3',
+    'description': "Hospital Management",
+    'website': 'https://www.github.com/adil6055',
+    'license': 'LGPL-3',
     'application': True
 }
